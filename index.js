288c68560b66bf96f66e2a2ee858e53170520501
Anthony Dinh <adinh182@gmail.com>
2017-08-23 07:38:42 -0400

    add initial files

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+const request = require('request');
+const rp = require('request-promise');
+const alexaSkillKit = require('alexa-skill-kit');
+const AlexaMessageBuilder = require('alexa-message-builder');
+
+var options = {
+    uri: 'http://api.forismatic.com/api/1.0/',
+    qs: { method: 'getQuote', format: 'json', key: '', lang: 'en' },
+    json: true
+};
+
+
+var propertiesObject = {method:'getQuote', format:'json', key:'', lang:'en'};
+var url = 'http://api.forismatic.com/api/1.0/'
+
+
+
+
+
+var SKILL_NAME = 'Motivational Quotes'
+
+exports.handler = function (event, context) {
+    alexaSkillKit(event, context, parsedMessage => {
+
+       return  rp({ url: url, qs: propertiesObject }).then(body =>{
+            console.log(body);
+            var json = JSON.parse(body);
+            console.log(json);
+            var quote = json.quoteText;
+            var author = json.quoteAuthor;
+            var speechOutput = quote + ' by ' + author;
+
+            return new AlexaMessageBuilder().addText(speechOutput).get();
+        })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+
+
+
+
+    });
+
+};
+
+
+
